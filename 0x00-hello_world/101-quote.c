09ffc3c50ec259083b79af87cf732205735f6584
Joyonos <Joyonos@gmail.com>
2023-09-09 05:59:19 -0700

    Print exactly the file followed by a new line to the standard error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,10 @@
-iinclude < stdio.h >
+#include <unistd.h>
 /**
  * main-this prints exactly the input on it
  * Return:always1
  */
 int main(void)
 {
-	string Dora;
-
-	fwrite("\"and that piece of art is useful" - Dora) Korpar, 2015 - 10 - 19\n");
-	return (1);
+	write("and that piece of art is useful" - Dora Korpar, 2015 - 10 - 19 \n ",);
+	Return (1);
 }
