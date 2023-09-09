a2d72c18d3a8e7580114d05bc1c7656f7a4b9e76
Joyonos <Joyonos@gmail.com>
2023-09-09 01:50:09 -0700

    Print exactly the filename followed by a new line to the standard error

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+iinclude < stdio.h >
+/**
+ * main-this prints exactly the input on it
+ * Return:always1
+ */
+int main(void)
+{
+	string Dora;
+
+	fwrite("\"and that piece of art is useful" - Dora) Korpar, 2015 - 10 - 19\n");
+	return (1);
+}
