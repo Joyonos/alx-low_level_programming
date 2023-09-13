bdb2072996926a02ddb12c6b7a91ed382e81b425
Joyonos <Joyonos@gmail.com>
2023-09-12 22:51:47 -0700

    Function that checks for lowercase

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ *int _islower-entry point
+ *int-function that checks for lowercase character
+ * @c:to check if its lowercase
+ * Return:0-if lowercase, 1-if not
+ */
+
+int _islower(int c);
+
+}
+	if (c >= 'a' && c <= 'z')
+	{
+	return (1);
+	}
+	else
+	{
+	return (0);
+	}
+}
